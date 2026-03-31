0a2db8d8c8a30b586b3708527c21a71f180fc383
Danijel-James Wynyard <danijel@repasscloud.com>
2026-04-01 01:55:22 +1100

    feat: Update CurrencyCode enum to include additional currency options
    v2026.04.1-rc1b

--- src/Aethon.Shared/Enums/CurrencyCode.cs
+++ src/Aethon.Shared/Enums/CurrencyCode.cs
@@ -6,5 +6,20 @@ public enum CurrencyCode
     USD = 2,
     EUR = 3,
     GBP = 4,
-    NZD = 5
+    NZD = 5,
+    CAD = 6,
+    CHF = 7,
+    JPY = 8,
+    CNY = 9,
+    HKD = 10,
+    SEK = 11,
+    KRW = 12,
+    SGD = 13,
+    NOK = 14,
+    MXN = 15,
+    INR = 16,
+    RUB = 17,
+    ZAR = 18,
+    TRY = 19,
+    BRL = 20
 }
